b23d2a43b724f737df77ce435397d0afa2364c1c
Shrisha31 <144545789+Shrisha31@users.noreply.github.com>
2023-09-09 20:32:51 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,10 @@
+def isleapyear(year):
+  if(year % 4==0 and year % 100 !=0) or year % 400==0:
+    return True
+  else:
+    return False
+year = 2008
+if isleapyear(year):
+  print(year,"is a leap year,")
+else:
+  print(year,"is not a leap year ")
